199035dd8ffcb58af843f0118d235d5e55b54fdf
ashmeetsmonga <ashmeetsmonga@gmail.com>
2020-01-13 18:31:31 +0530

    Updates

---
+++ precache-manifest.d7d1782e6cade7f981aa90c6a261ce1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51131b038aa91c96339983a5929abcf",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "406d818d83e68d50d89b",
+    "url": "/todo-react/static/css/main.5bab9e06.chunk.css"
+  },
+  {
+    "revision": "13a16e795fde9e603828",
+    "url": "/todo-react/static/js/2.500f24f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-react/static/js/2.500f24f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "406d818d83e68d50d89b",
+    "url": "/todo-react/static/js/main.6ceb11ad.chunk.js"
+  },
+  {
+    "revision": "d3eef746a5dd3d47c957",
+    "url": "/todo-react/static/js/runtime-main.21d747d4.js"
+  },
+  {
+    "revision": "7677c11e78f7a570bc284eab0138310a",
+    "url": "/todo-react/static/media/bg.7677c11e.jpg"
+  }
+]);
